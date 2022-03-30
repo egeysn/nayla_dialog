8495c66686725980ff4ad20311554a00d957e17b
egeysn <yasin.ege@naylalabs.com>
2022-03-30 14:11:46 +0300

    Add nayla alert dialog.

--- lib/nayla_dialog.dart
+++ lib/nayla_dialog.dart
@@ -1,7 +1,35 @@
 library nayla_dialog;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+import 'package:flutter/material.dart';
+
+
+class NaylaDialog {
+  static  showCustomAlertBox({
+    required BuildContext context,
+    required Widget customWidget,
+  }) {
+    return showDialog(
+        context: context,
+        builder: (context) {
+          return AlertDialog(
+            shape: const RoundedRectangleBorder(
+              borderRadius: BorderRadius.all(Radius.circular(15)),
+            ),
+            content: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                customWidget,
+                MaterialButton(
+                  color: Colors.white30,
+                  child: const Text('close alert'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            ),
+            elevation: 10,
+          );
+        });
+  }
 }
